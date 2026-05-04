9f225b087e5eebfc0acee63d76ca8092adf94203
1samanthacheng <1samanthacheng@gmail.com>
2026-05-04 11:55:08 -0400

    Update footer.tsx
    increased padding

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,7 +25,7 @@ function ArrowUpIcon({ className }: { className?: string }) {
 
 export function Footer() {
   return (
-    <footer className="px-8 md:px-16 lg:px-24 xl:px-32">
+    <footer className="px-12 md:px-24 lg:px-40 xl:px-52">
       {/* Top Divider */}
       <div className="h-[3px] bg-primary" />
 
